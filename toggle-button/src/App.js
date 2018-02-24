c4f2b356397a85f92ce430805893df5ba8aa685b
Daniel Parkin <danielparkin1@gmail.com>
2018-02-24 16:48:59 +0000

    3. write compound components

--- toggle-button/src/App.js
+++ toggle-button/src/App.js
@@ -20,18 +20,46 @@ function Switch({ on, className = '', ...props }) {
   )
 }
 
+function ToggleOn({on, children}) {
+  return on ? children : null
+}
+
+function ToggleOff({on, children}) {
+  return on ? null : children
+}
+
+function ToggleButton({on, toggle, ...props}) {
+  return <Switch on={on} onClick={toggle} {...props} />
+}
+
+// React.Children.map is a special mapping function for mapping over React children elements
+// map over each of the children and create a clone with the parent components state passed in
 class Toggle extends Component {
+  static On = ToggleOn
+  static Off = ToggleOff
+  static Button = ToggleButton
   static defaultProps = {onToggle: () => {}}
   state = {on: false}
   toggle = () => this.setState(({on}) =>  ({on: !on}), () => {
     this.props.onToggle(this.state.on)
   })
   render() {
-    const {on} = this.state
-    return <Switch on={on} onClick={this.toggle} />
+    const children = React.Children.map(
+      this.props.children,
+      child =>
+        React.cloneElement(child, {
+          on: this.state.on,
+          toggle: this.toggle,
+        }),
+    )
+    return <div>{children}</div>
   }
 }
 
+// the Toggle component is a compound component
+// compound components have one component at the top level with children, who all share some
+// implicit state
+// in this case, the "on" state
 class App extends Component {
   render() {
     return (
@@ -42,7 +70,12 @@ class App extends Component {
         flexDirection: 'column',
         textAlign: 'center',
       }}>
-        <Toggle onToggle={on => console.log('toggle', on)} />
+        <Toggle onToggle={on => console.log('toggle', on)}
+        >
+          <Toggle.On>The button is on</Toggle.On>
+          <Toggle.Button />
+          <Toggle.Off>The button is off</Toggle.Off>
+        </Toggle>
       </div>
     );
   }
